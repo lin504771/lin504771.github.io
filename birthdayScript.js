5494146a95634161e45a54a6b1db974490e5b4c6
lin504771 <62493070+lin504771@users.noreply.github.com>
2020-04-29 11:45:21 -0500

    Add files via upload

--- birthdayScript.js
+++ birthdayScript.js
@@ -1,1 +1,10 @@
 alert("Happy 19th Birthday Michaela!");
+
+let arf = document.getElementById('help');
+arf.onclick = 
+function() {
+    let coolio = document.createElement('img');
+    coolio.src = "confetti.png";
+
+    document.body.appendChild(coolio);
+}
